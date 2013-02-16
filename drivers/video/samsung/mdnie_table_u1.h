jeboo/kernel_JB_ZSLS6_i9100

Added MDNIE sharpness toggle (gokhanmoral)

drivers/video/samsung/mdnie_table_u1.h
@@ -995,7 +995,7 @@ static const unsigned short tune_warm_outdoor[] = {
 	END_SEQ, 0x0000,
 };
 
-struct mdnie_tuning_info etc_table[CABC_MAX][OUTDOOR_MAX][TONE_MAX] = {
+static struct mdnie_tuning_info etc_table[CABC_MAX][OUTDOOR_MAX][TONE_MAX] = {
 	{
 		{
 			{"NORMAL",		NULL},
@@ -1010,7 +1010,7 @@ struct mdnie_tuning_info etc_table[CABC_MAX][OUTDOOR_MAX][TONE_MAX] = {
 	}
 };
 
-struct mdnie_tuning_info tuning_table[CABC_MAX][MODE_MAX][SCENARIO_MAX] = {
+static struct mdnie_tuning_info tuning_table[CABC_MAX][MODE_MAX][SCENARIO_MAX] = {
 	{
 		{
 			{"DYNAMIC_UI",		tune_dynamic_ui},
@@ -1052,7 +1052,7 @@ struct mdnie_tuning_info tuning_table[CABC_MAX][MODE_MAX][SCENARIO_MAX] = {
 	}
 };
 
-struct mdnie_tuning_info camera_table[OUTDOOR_MAX] = {
+static struct mdnie_tuning_info camera_table[OUTDOOR_MAX] = {
 	{"CAMERA",		tune_camera},
 	{"CAMERA_OUTDOOR",	tune_camera_outdoor},
 };

jeboo/kernel_JB_I777

drivers/video/samsung/mdnie_table_u1.h
@@ -995,7 +995,7 @@ static const unsigned short tune_warm_outdoor[] = {
 	END_SEQ, 0x0000,
 };
 
-struct mdnie_tunning_info etc_table[CABC_MAX][OUTDOOR_MAX][TONE_MAX] = {
+static struct mdnie_tunning_info etc_table[CABC_MAX][OUTDOOR_MAX][TONE_MAX] = {
 	{
 		{
 			{"NORMAL",		NULL},
@@ -1010,7 +1010,7 @@ struct mdnie_tunning_info etc_table[CABC_MAX][OUTDOOR_MAX][TONE_MAX] = {
 	}
 };
 
-struct mdnie_tunning_info tunning_table[CABC_MAX][MODE_MAX][SCENARIO_MAX] = {
+static struct mdnie_tunning_info tunning_table[CABC_MAX][MODE_MAX][SCENARIO_MAX] = {
 	{
 		{
 			{"DYNAMIC_UI",		tune_dynamic_ui},
@@ -1052,7 +1052,7 @@ struct mdnie_tunning_info tunning_table[CABC_MAX][MODE_MAX][SCENARIO_MAX] = {
 	}
 };
 
-struct mdnie_tunning_info camera_table[OUTDOOR_MAX] = {
+static struct mdnie_tunning_info camera_table[OUTDOOR_MAX] = {
 	{"CAMERA",		tune_camera},
 	{"CAMERA_OUTDOOR",	tune_camera_outdoor},
 };